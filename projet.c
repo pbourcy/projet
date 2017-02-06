94835ebef500fc04cf7cca4ae7140d8bb6ba20a3
pbourcy <pierre.bourcy@etu.univ-amu.fr>
2017-02-06 17:42:09 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -1,4 +1,70 @@
 
+/* INITIALISATION */
+
+#define TRUE 1 
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int echiquier[24] = { 0 , 2 , 0 , 0 , 0 , 0 , -5 , 0 , -3 , 0 , 0 , 0 , 5 , -5 , 0 , 0 , 0 , 3 , 0 , 5 , 0 , 0 , 0 , -2 } ;
+int echiquierVirtuel[24] = { 0 } ;
+
+/*
+int echiquier[24] = { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ;
+echiquier[1] = 2 ;
+echiquier[6] = -5 ;
+echiquier[8] = -3 ;
+echiquier[12] = 5 ;
+echiquier[13] = -5 ;
+echiquier[17] = 3 ;
+echiquier[19] = 5 ;
+echiquier[24] = -2 ;
+*/
+
+int pion_mort , pionsRentreAI , pionsRentreJoueur ;
+int pionGagneJoueur , pionGagneAI ;
+
+int backtrack ( int prof ) 
+{
+	tTuple CP = tableauTuple( j ) ; // C'pas ça mais en gros la liste des coups possible donc on appelle la fonction jouable pour l'ordi. 
+	int tmp = min ; // ou max faut voir 
+
+	if ( prof < prof_max ) 
+	{
+		for (i=0;i<longueurCP;i++) // for c € C :  pour i dans la liste de coup, donc on fait une boucle qui donne pour chaque coup
+		{
+			tTuple coup = CP[i].int2val ;
+			jouer(c) //on va ajouter 1 ou -1 a  la case i+de 
+			
+			v = backtrack( prof + 1 );
+			tmp = min_max( tmp , v ) ;
+
+			dejouer(c) //on va retirer 1 ou -1 a la case i+de
+		}
+		return tmp
+	}
+	else //profmax ~ feuille
+	{
+		v = evaluation //echiquier ( donc fonction evaluation())
+		return v ;
+	}
+}
+
+int play ( int caseDepart , int caseArrive , int camp )
+{
+	int echiquierVirtuel[caseDepart] = -camp ;
+	int echiquierVirtuel[caseArrive] = camp ;
+	return * echiquierVirtuel ;
+}
+
+int deplay ( int caseDepart , int caseArrive , int camp )
+{
+	int echiquierVirtuel[caseDepart] = camp ;
+	int echiquierVirtuel[caseArrive] = -camp ;
+	return * echiquierVirtuel ;
+}
+
 int deplacement_possible(int de, int i, int camp) {
 	if ( echiquier[i+de] >= camp || i + de <= 24) {
 		return 1 ;
@@ -21,34 +87,75 @@ tTuple tableauTuple ( int j ) {
 	{
 		CP[i] = tuplezero ; 
 	}
-	return CP ; 
+	return * CP ; //Un truc chelou, return CP fonctionne pas ici mais dans jouable oui
 }
 
-tTuple Jouable(int de , int j ) //CP = coup possible
+tTuple Jouable( int de , int j ) //CP = coup possible
 	{
 		tTuple CP = tableauTuple( j ) ; //j = 15 trés souvent
-		if ( pion_mort >= 1 ) {
-			i = 1 ; 
-			a = deplacement_possible( de, i ) ;
-			if ( a == 1 ) {
-				CP.int1Val = CP.int1Val + i ;
-				CP.int2Val = CP.int2Val + i + de ;
+
+		if ( pion_mort >= 1 )
+		{
+			int i = 1 ; 
+			int a = deplacement_possible( de, i, 1 ) ; 
+			if ( a == 1 ) 
+				{
+				if (camp == 1)
+				{
+					CP[0].int1Val = 0 ;
+					CP[0].int2Val = i + de ; //y'a un truc qui va pas avec le cp.int2val
+				}
+				else // l'ia psk -1 
+				{
+					CP[0].int1Val = 25 ;
+					CP[0].int2Val = 25 - i - de ;
+				}
 		}
-		else {
+		else 
+		{
+			/*
 			for( i = 1 ; i<25 ; i++ ) 
 			{
-				if ( echiquier [ i + 1 ] > 0 ) 
+				if ( echiquier[ i + 1 ] > 0 ) 
 				{
-					a = deplacement_possible ( de , i ) ;
+					a = deplacement_possible ( de , i, 1 ) ; 
 					if ( a == 1 ) 
 					{
-						CP.int1Val = CP.int1Val + i ;
-						CP.int2Val = CP.int2Val + i + de ;
+						if (camp == 1) 
+						{
+							CP[i].int1Val = i ;
+							CP[i].int2Val = i + de ;
+						}
+						else 
+						{
+							CP[i].int1Val = i ;
+							CP[i].int2Val = 25 - i - de ;
+						}
 					}
 				}
 			}
+			*/
+			for( i = 1 ; i<25 ; i++ ) 
+			{
+				if ( (echiquier[ i + 1 ] > 0) || (camp == 1) ) 
+				{
+					a = deplacement_possible ( de , i, 1 ) ; 
+					if ( a == 1 ) 
+					{
+						CP[i].int1Val = i ;
+						CP[i].int2Val = i + de ;	
+					}
+				}
+				else if ((echiquer[ i + 1 ] < 0) || (camp == - 1) )
+					a = deplacement_possible ( de , i, -1 ) ; 
+					if ( a == -1 ) 
+					{
+						CP[i].int1Val = 25 - i ;
+						CP[i].int2Val = 25 - i - de ;	
+					}
+			}
 		}
-		return CP ;
+		return * CP ;
 	}
 
 int conditionPhaseFinal( void ) {
@@ -60,9 +167,14 @@ int conditionPhaseFinal( void ) {
 }
 
 int evaluation ( int c , int camp , int pionsRentreAI , int pionsRentreJoueur ) {
-	int echiquierVirtuel = echiquier ; 
+	
+	/*On peut pas copier des arrays/tableau juste en faisant l'un égale l'autre, donc on utilise la fonction memcpy*/
+	memcpy(echiquier, echiquierVirtuel, sizeof(echiquier)) ;
+	/*echiquierVirtuel = echiquier ; */
+	
+	int i ;
 	int valeurEchiquier = 0 ; 
-	if ( echiquierVirtuel [ c ] == camp * ( -1 ) ) echiquierVirtuel[ c ] = camp ; 
+	if ( echiquierVirtuel[ c ] == camp * ( -1 ) ) echiquierVirtuel[ c ] = camp ; 
 	for(i = 1 ; i < 25 ; i++ ) //24 valeurs 
 		{
 			if ( ( echiquierVirtuel[ i ] < 0 ) && ( echiquierVirtuel[ i ] > 0 ) ) 
@@ -86,28 +198,38 @@ void conditionVictoire( int pionGagne , int camp ) {
 	if ( pionGagne == 15 ) printf("le camp %i a gagne", &camp ) ;
 }
 
-tTuple concatenation( tTuple CP1 , tTuple CP2 , tTuple CP ) {
+tTuple concatenation( tTuple CP1[] , tTuple CP2[] , tTuple CP[] ) //erreur d'écriture besoin pointeur et non tableau
+{
 	int i ;
 	for( i == 1 ; i < 31 ; i++ )
 	{
 		if (i>=16) CP[ i ] = CP2[i] ;
 		else CP[ i ] = CP1[ i - 15 ] ;
 	}
-	return CP ;
+	return * CP ;
 }
 
-tTuple coupJoueur( int de1 , int de2 , int deJoue )
+tTuple coupJoueur( int de1 , int de2 , int deJoue ) 
+{
+	int j = 15 ;
 	int l ; 
+	/*
 	tTuple CPvide = tableauTuple( 15 ) ;
-	tTuple CP1 = Jouable( de1 , CPvide ) ;
-	tTuple CP2 = Jouable( de2 , CPvide ) ; 
-	if ( deJoue == 0 ) {
+	tTuple CP1 = Jouable( int de1 , tTuple CPvide ) ;   //ON PEUT PAS CAR JOUABLE ACCEPTE QUE ( INT DE, INT J )
+	tTuple CP2 = Jouable( int de2 , tTuple CPvide ) ; 	//ON PEUT PAS CAR JOUABLE ACCEPTE QUE ( INT DE, INT J )
+	*/
+	tTuple CP1 = Jouable( de1 , j ) ; 
+	tTuple CP2 = Jouable( de2 , j ) ;
+
+	if( deJoue == 0 ) 
+		{
 		l = 30 ;
 		tTuple CP = tableauTuple( 30 ) ; 
-		CP = concatenation ( CP1 , CP2 , CP ) ;
+		CP = concatenation( CP1 , CP2 , CP ) ;
+		}
 	else 
 	{
-		l = 15
+		l = 15 ;
 		tTuple CP = tableauTuple( 15 ) ; 
 		if( deJoue == de1 ) 
 		{
@@ -120,10 +242,10 @@ tTuple coupJoueur( int de1 , int de2 , int deJoue )
 			CP = CP1 ;
 		}
 	}
-
+	int i ;
 	for( i = 0 ; i < l+1 ; i++ ) printf("N°%i - Case depart : %i et case d'arrive : %i", i , CP[i].int1Val , CP[i].int2Val ) ;
 
-
+	int x ;
 	while(TRUE) //define TRUE 1 
 	{
 		scanf("Entrer le numero du coup que vous voulez jouer", &x ) ; 
@@ -134,44 +256,5 @@ tTuple coupJoueur( int de1 , int de2 , int deJoue )
 		}
 		printf(" Coup invalide, recommencez " ) ;
 	}
-
-/* INITIALISATION */
-
-int echiquier = {24}
-int echiquierVirtuel = {24}
-
-echiquier[1] = 2 ;
-echiquier[6] = -5 ;
-echiquier[8] = -3 ;
-echiquier[12] = 5 ;
-echiquier[13] = -5 ;
-echiquier[17] = 3 ;
-echiquier[19] = 5 ;
-echiquier[24] = -2 ;
-
-int pion_mort , pionsRentreAI , pionsRentreJoueur ;
-int pionGagneJoueur , pionGagneAI ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
